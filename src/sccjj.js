c48301c1c3f397f67b62739100858896e65b206f
Yordi <yorditjordin@gmail.com>
2023-04-01 18:06:12 +0300

    summary

--- src/sccjj.js
+++ src/sccjj.js
@@ -47,6 +47,61 @@ function addItem(e) {
   }
 }
 
+function clearCompleted() {
+  tasks = tasks.filter((task) => !task.completed); // Filter out completed tasks from the tasks array
+  updateTasksIndexes(); // Update the task indexes
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTasks(); // Render the updated todo list
+}
+
+function editTask(index, newDescription) {
+  tasks[index].description = newDescription; // Update the task description
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTasks(); // Render the updated todo list
+}
+
+function updateTasksIndexes() {
+  tasks.forEach((task, index) => {
+    task.index = index; // Update the task index
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1); // Remove the task at the given index from the tasks array
+  updateTasksIndexes(); // Update the task indexes
+  localStorage.setItem('tasks', JSON.stringify(tasks));// Save the updated tasks array to local storage
+  displayTasks(); // Render the updated todo list
+}
+
+// This function is called when a task's description is double-clicked and allows the user to edit the description
+function editTaskDescription(e) {
+  // Find the closest li element to the double-clicked element
+  const li = e.target.closest('li');
+  // Find the span element within the li element
+  const span = li.querySelector('span');
+  // Get the value of the "index" attribute of the input element within the li element
+  const { index } = li.querySelector('input').dataset;
+  // Allow the user to edit the task description by setting the "contentEditable" attribute of the span element to true and focusing on it
+  span.setAttribute('contentEditable', true);
+  span.focus();
+  // When the user presses the Enter key, blur the span element to save the changes
+  span.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      span.blur();
+    }
+  });
+  // When the span element loses focus, check if the new description is empty, and if not, update the task description
+  span.addEventListener('blur', () => {
+    const newDescription = span.textContent.trim();
+    if (newDescription === '') {
+      deleteTask(index);
+    } else {
+      editTask(index, newDescription);
+    }
+  });
+}
 // This function is called when a user clicks on a task or its associated icons
 function toggleItem(e) {
   // Find the closest li element to the clicked element
@@ -95,9 +150,9 @@ function toggleItem(e) {
 // Add event listeners to various elements on the page
 form.addEventListener('submit', addItem);
 todoList.addEventListener('click', toggleItem);
-enterIcon.addEventListener('click', addItem);
+todoList.addEventListener('dblclick', editTaskDescription);
+archiveBtn.addEventListener('click', clearCompleted);
 
+enterIcon.addEventListener('click', addItem);
 // Render the initial tasks on the page when it loads
-window.onload = (e) => {
-  displayTasks();
-};
+displayTasks();
